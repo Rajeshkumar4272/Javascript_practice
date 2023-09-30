ec700f3391819ed9cece13e2509a4cf295372199
Rajesh_Kumar <rk914479@gmail.com>
2023-09-30 19:28:33 +0530

    first commit

---
+++ testanswer.js
@@ -0,0 +1,39 @@
+
+
+// function getlength( arr = [2,3[4,5[6]]]){
+//     const newArray = arr.flat(Infinity)
+//     return newArray.length
+// }
+
+// function obj(arr = ["A", "B", "A", "A", "A"]) {
+//     let b = {
+//         // this is my object
+//     }
+//     arr.forEach(function (value) {
+//         if (b.hasOwnProperty(value)) {
+           
+//             b[value] += 1
+//         }
+//         else {
+//             b[value] = 1
+//         }
+//     }
+
+//     )
+// console.log(b);
+//     return b
+
+// }
+
+
+arr= ['Simran kaur', 'Taniya saini', 'Rajesh kumar', 'Aman mishra']
+arr.sort(function(a , b){
+    const firstsurname = a.split(' ') [1]
+    const secondsurname = b.split(' ') [1]
+    if(firstsurname.length === secondsurname.length){
+        return firstsurname.localeCompare(secondsurname)
+    }
+    return firstsurname.length < secondsurname.length  ? -1 :1 
+})
+
+
