80d43e72de9adc8769a6ff7b3a6aa01988dfeb2b
Rajesh_Kumar <rk914479@gmail.com>
2023-10-06 08:15:46 +0530

    sorting program added

--- sorting.js
+++ sorting.js
@@ -1,7 +1,27 @@
-const myArr =[12,45,2,5,1,73,8]
-myArr.sort((a,b) =>{
-    if (a>b) return 1
-    else if (a<b) return -1
+const myArr = [12, 45, 2, 5, 1, 73, 8]
+myArr.sort((a, b) => {
+    if (a > b) return 1
+    else if (a < b) return -1
     else return 0
 })
 console.log(myArr);
+
+console.log("**************** sorting with loop ***********");
+
+let arr = [2, 4, 1, 5, 7, 3, 9, 10, 22, 11]
+for (let i = 0; i < arr.length; i++) {
+    let temp;
+    // console.log("check");
+    for (let j = i; j < arr.length; j++) {
+        // console.log("hello");
+        if (arr[i] > arr[j]) {
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+        }
+    }
+    // console.log(arr)
+
+}
+
+console.log(arr);
